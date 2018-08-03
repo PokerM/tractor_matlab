a553ef1427b7eece9560e0bb20b45cf0589cefe9
PokerM <32540033+PokerM@users.noreply.github.com>
2018-08-03 19:42:32 +0800

    change speed con

--- speed_error_control/speed_con.m
+++ speed_error_control/speed_con.m
@@ -1,30 +1,25 @@
-function [res, fval] = speed_con(c_v, c_fai, c_theta, e_theta)
+function [res,dx1,dy1,dfai1,dx2,dy2,dfai2] = speed_con(c_v, c_fai, c_theta, e_theta)
 T = 0.1;
-v_theta = 0.2356;
-Q = [1,1,1];
-R = -1;
+if c_theta < e_theta
+    v_theta = 0.2356;
+else
+    v_theta = -0.2356;
+end
 N = 10;
 t = T / N;
 L = 1.06;
 dx1 = 0;
-dx2 = 0; 
+dx2 = 0;
 dy1 = 0;
-dy2 = 0; 
-dfai1 = 0; 
+dy2 = 0;
+dfai1 = 0;
 dfai2 = 0;
 syms v
-% for i = 1 : N
-%     theta(i) = c_theta + v_theta * i * t;
-%     if theta(i) < e_theta || theta(i) > e_theta
-%         theta(i) = e_theta;
-%     end
-% end
 theta = c_theta;
 for i = 1 : N
     dfai1 = dfai1 + c_v / L * tan(e_theta) * i * t;
     dx1 = dx1 + c_v * cos(c_fai + dfai1);
     dy1 = dy1 + c_v * sin(c_fai + dfai1);
-
     theta = theta + v_theta * t;
     if theta < e_theta || theta > e_theta
         theta = e_theta;
@@ -35,5 +30,8 @@ for i = 1 : N
 end
 err = 5 * (dx1 - dx2)^2 + 5 * (dy1 - dy2)^2 + (dfai1 - dfai2)^2;
 J =  err + 1 * v ^ -2;
-[res,fval] = fmincon(matlabFunction(J), 0, [],[],[],[],0,2.4);
+[res] = fmincon(matlabFunction(J), 0, [],[],[],[],0,2.4);
+dx2 = double(subs(dx2, v, res));
+dy2 = double(subs(dy2, v, res));
+dfai2 = double(subs(dfai2, v, res));
 end
